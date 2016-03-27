e5d07d9115565b06fcf06ee366b76ec816cae565
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2016-03-27 23:24:11 +0300

    changed from base64 encrypt to utf-8

--- GameCym/app/src/main/java/com/project/ingprog/gamecym/AESEncryption.java
+++ GameCym/app/src/main/java/com/project/ingprog/gamecym/AESEncryption.java
@@ -4,8 +4,6 @@ package com.project.ingprog.gamecym;
  * Created by danyh on 3/27/2016.
  */
 
-import org.apache.commons.codec.binary.Base64;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -27,10 +25,11 @@ public class AESEncryption {
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
             byte[] encrypted = cipher.doFinal(value.getBytes());
-            System.out.println("encrypted string: "
-                    + Base64.encodeBase64String(encrypted));
+            //System.out.println("encrypted string: "
+            //        + Base64.encodeBase64String(encrypted));
 
-            return Base64.encodeBase64String(encrypted);
+            //return Base64.encodeBase64String(encrypted);
+            return new String(encrypted);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -52,7 +51,9 @@ public class AESEncryption {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+            //byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            byte[] original = cipher.doFinal(encrypted.getBytes("UTF-8"));
 
             return new String(original);
         } catch (Exception ex) {
